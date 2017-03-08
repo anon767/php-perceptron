6c671458e19c2e4bdbd38cc7aea388cb7654a981
tg <tg@cinetic21.de>
2017-03-08 13:12:59 +0100

    done

--- src/Network.php
+++ src/Network.php
@@ -10,7 +10,7 @@ class Network
     protected $layers = [];
     protected $weights = [];
     protected $activationFunction;
-    protected $eta = 0.5;
+    protected $eta = 0.001;
 
     /**
      * Network constructor.
@@ -49,18 +49,24 @@ class Network
      */
     protected function getRandomWeight()
     {
-        return 1;
+        return ((mt_rand(0, 1000) / 1000) - 0.5) / 2;
     }
 
-    /**
-     * maybe remove the while loop to prevent overfitting
-     * @param $input
-     * @param $output
-     */
-    public function train($input, $output)
+
+    public function trainData($train, $epochs)
     {
-        $actualOutput = $this->calculate($input);
-        $this->backPropagate($actualOutput, $output);
+
+        for ($i = 0; $i < $epochs; $i++) {
+            $sum_error = 0;
+            $eta = $this->eta;
+            shuffle($train);
+            for ($j = 0; $j < count($train); $j++) {
+                $actualOutput = $this->calculate([$train[$j][0], $train[$j][1], $train[$j][2], $train[$j][3]]);
+                $sum_error += pow($actualOutput[0] - $train[$j][4], 2) + pow($actualOutput[1] - $train[$j][5], 2) + pow($actualOutput[2] - $train[$j][6], 2);
+                $this->backPropagate($actualOutput, [$train[$j][4], $train[$j][5], $train[$j][6]]);
+            }
+            printf("epoch=%d lrate=%.5f error=%.3f\r\n", $i, $eta, $sum_error);
+        }
 
     }
 
@@ -103,9 +109,9 @@ class Network
                 }
                 $this->layers[$i]->setNode($nodeID, $this->activationFunction->activate($sum));
                 $this->layers[$i]->setNodePure($nodeID, $sum);
-
             }
         }
+
         return $this->layers[count($this->layers) - 1]->getNodes();
     }
 
@@ -119,39 +125,35 @@ class Network
         if (count($output) != count($desiredOutput))
             throw new \InvalidArgumentException();
 
+
         for ($i = 0; $i < count($output); $i++) {
-            //added: multiplying derivative of current node to difference
-            /* $this->layers[count($this->layers) - 1]->setError($i,
-                 $this->activationFunction->derivativeActivate($output[$i]) * ($desiredOutput[$i] - $output[$i]));*/
-            $this->layers[count($this->layers) - 1]->setError($i, ($desiredOutput[$i] - $output[$i]));
+            $this->layers[count($this->layers) - 1]->setError($i, ($desiredOutput[$i] - $output[$i])
+                * $this->activationFunction->derivativeActivate($output[$i]));
         }
 
-        for ($i = count($this->layers) - 2; $i >= 0; $i--) {
+        for ($i = count($this->layers) - 2; $i >= 1; $i--) {
             foreach ($this->layers[$i]->getNodes() as $nodeID => $node) {
                 $sum = 0;
                 foreach ($this->layers[$i + 1]->getNodes() as $predNodeID => $predNode) {
-                    $sum += $this->layers[$i + 1]->getError($predNodeID) * $this->layers[$i]->getWeights()[$nodeID][$predNodeID];
+                    $sum += ($this->layers[$i + 1]->getError($predNodeID) * $this->layers[$i]->getWeights()[$nodeID][$predNodeID]);
                 }
-                //added: multiplying derivative of current node to difference
-                $this->layers[$i]->setError($nodeID, $sum);
+                $this->layers[$i]->setError($nodeID, $sum * $this->activationFunction->derivativeActivate($node));
             }
+
         }
+
 
         for ($i = 1; $i < count($this->layers); $i++) {
             $tempWeights = [];
             foreach ($this->layers[$i]->getNodes() as $nodeID => $node) {
                 foreach ($this->layers[$i - 1]->getNodes() as $prevNodeID => $prevNode) {
                     $tempWeights[$prevNodeID][$nodeID] = $this->layers[$i - 1]->getWeights()[$prevNodeID][$nodeID] +
-                        $this->eta * $this->layers[$i]->getError($nodeID) * $this->activationFunction->derivativeActivate($node);
-                    var_dump($this->layers[$i]->getError($nodeID));
+                        $this->eta * $this->layers[$i]->getError($nodeID) * $prevNode;
                 }
             }
-
-            exit;
             $this->layers[$i - 1]->setWeights($tempWeights);
         }
-
-
+        $this->eta *= 0.999999;
     }
 
     /**
--- test.php
+++ test.php
@@ -4,40 +4,37 @@ use NeuralNetwork\Network;
 use NeuralNetwork\ActivationFunction\TanhFunction;
 
 
-echo "<pre>";
 
-$n = new Network([4, 5, 3], new TanhFunction());
+$n = new Network([4, 4, 3], new TanhFunction());
 
 $fileContents = str_replace("Iris-virginica", "-1,-1,1", str_replace("Iris-versicolor", "-1,1,-1", str_replace("Iris-setosa", "1,-1,-1", file_get_contents("TestData/IRIS.txt"))));
 
 $testCases = explode("\n", $fileContents);
 
+$trainingset = [];
 
-$j = 0;
+
+for ($i = 0; $i < count($testCases); $i++) {
+    $params = explode(",", $testCases[$i]);
+    $trainingset[] = [$params[0], $params[1], $params[2], $params[3], $params[4], $params[5], trim($params[6])];
+}
+
+$n->trainData($trainingset, 5000);
+/*$j = 0;
 do {
     shuffle($testCases);
-    for ($i = 0; $i < 146; $i++) {
+    for ($i = 0; $i < 80; $i++) {
         $params = explode(",", $testCases[$i]);
         $n->train([$params[0], $params[1], $params[2], $params[3]], [$params[4], $params[5], $params[6]]);
     }
     $j++;
-} while ($j < 2);
 
-echo "<hr>";
-//5.1,3.8,1.5,0.3,Iris-setosa
-$testCase = $n->calculate([5.1, 3.8, 1.5, 0.3]);
-print_r("Iris Setosa: [1,-1,-1] <br>");
-print_r($testCase);
-echo "<hr>";
-//7.0,3.2,4.7,1.4,Iris-versicolor
-$testCase = $n->calculate([7.0, 3.2, 4.7, 1.4]);
-print_r("Iris Versicolor [-1,1,-1]: <br>");
-print_r($testCase);
-echo "<hr>";
-//7.2,3.0,5.8,1.6,Iris-virginica
-$testCase = $n->calculate([7.2, 3.0, 5.8, 1.6]);
-print_r("Iris Virginica: [-1,-1,1]<br>");
-print_r($testCase);
-echo "<hr>";
+} while ($j < 100);
+*/
 
-//$n->getStatus();
+echo "-1,1,-1";
+print_r($n->calculate([7.0, 3.2, 4.7, 1.4]));
+echo "\r\n1,-1,-1";
+print_r($n->calculate([5.1, 3.8, 1.5, 0.3]));
+echo "\r\n-1,-1,1";
+print_r($n->calculate([7.2, 3.0, 5.8, 1.6]));
